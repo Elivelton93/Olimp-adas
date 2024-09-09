eb6354663e93b9d60cff36d1f4f27c4dbee18495
Elivelton <162604424+Elivelton93@users.noreply.github.com>
2024-09-09 09:22:04 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela se tornou a mais jovem medalhista olímpica da história do Brasil, inspirando uma nova geração de skatistas. Com seus movimentos fluidos e criativos, Rayssa revolucionou o skate feminino e se tornou um ícone da cultura jovem.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8",
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro na categoria +78kg. Sua força e técnica impecável a tornaram uma das principais atletas do judô mundial, inspirando jovens judocas a seguirem seus passos.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoka",
+
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros com suas performances incríveis. Nas Olimpíadas de Tóquio 2020, ela se tornou a primeira ginasta brasileira a conquistar uma medalha de ouro individual, e em Paris 2024, continuou a brilhar, conquistando mais medalhas. Sua força, determinação e carisma a tornaram um ícone do esporte brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginastica ginástica ouro",
+
+    }
+];
+
